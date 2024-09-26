63755483045508da5ae937614eb20840316bb8d9
alliseeisgold <gbaleme002@gmail.com>
2024-09-26 22:53:13 +0300

    Add other files

---
+++ src/test/java/com/alliseeisgoal/fractalflame/FractalflameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alliseeisgoal.fractalflame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FractalflameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
